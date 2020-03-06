6b7e1a444b5827ed14242ed864182dfe4fe00065
EvAlGodGood <Gooodfile@mail.ru>
2020-03-06 19:47:11 +0800

    Не справился с дз1. Не понял как изменить цвет canvas поэтому подменил еще одним потомком Sprite (схитрожопил =)). По убалению прибавления не разобрался как отледить нажатие левой правой кнопки мышки (хотел сделать просту уменьшая добавля элементы в отрисовку из готового массива). Вопросы в комментах для себя. но если есть время/желание эту кашу разгрести, то огромное спасибо.

--- src/circles/BackGroundCanvas.java
+++ src/circles/BackGroundCanvas.java
@@ -1,4 +1,51 @@
 package circles;
 
-public class BackGroundCanvas {
+import java.awt.*;
+//схитрим тк как не понял как менять конкретно цвет canvas
+//сделаем бэкграунд наследником Sprite только он будет отрисовываться первым перед отрисовкой шаров
+//и будет висеть на одном месте
+
+public class BackGroundCanvas extends Sprite{
+    protected int count = 0;
+    private  Color colorBG = new Color(
+            (int) (Math.random() * 255),
+            (int) (Math.random() * 255),
+            (int) (Math.random() * 255)
+    );
+
+    BackGroundCanvas() {
+        halfHeight = 120;
+        halfWidth = 120;
+    }
+
+//    public static void setColorBG(int i, int i1, int i2) {
+//        colorBG = (i, i1, i2);
+//    }
+
+
+    @Override
+    public void update(MainCanvas canvas, float deltaTime) { //определяем где на canvas отрисовать "фон"
+        count++;
+        if ((count % 150)==0) {
+            colorBG = new Color(
+                    (int) (Math.random() * 255),
+                    (int) (Math.random() * 255),
+                    (int) (Math.random() * 255));
+
+        }
+        x = canvas.getLeft() / 2;
+        y = canvas.getTop() / 2;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void render(MainCanvas canvas, Graphics g) { //готовим для отрисовке на canvas
+        g.setColor(colorBG);
+        g.fillRect((int) canvas.getLeft(), (int) canvas.getTop(),
+                (int) canvas.getWidth(), (int) canvas.getHeight());
+    }
+
 }
--- src/circles/Ball.java
+++ src/circles/Ball.java
@@ -1,4 +1,48 @@
 package circles;
 
-public class Ball {
+import java.awt.*;
+
+public class Ball extends Sprite { //класс наследуется от абстрактного класса Sprite
+    private final Color color = new Color( //задаем цвет
+            (int) (Math.random() * 255),
+            (int) (Math.random() * 255),
+            (int) (Math.random() * 255)
+    );
+    private float vX = (float) (100f + (Math.random() * 200f)); // рандомно задали скорость по х и y
+    private float vY = (float) (100f + (Math.random() * 200f));
+
+    Ball() {
+        halfHeight = 20 + (float) (Math.random() * 50f); //задаем размер Ball
+        halfWidth = halfHeight;
+    }
+
+    @Override
+    public void update(MainCanvas canvas, float deltaTime) { //определяем где на canvas отрисовать исходя из скорости и времени
+        x += vX * deltaTime;
+        y += vY * deltaTime;
+        if (getLeft() < canvas.getLeft()) { //отскок от левого края окна
+            setLeft(canvas.getLeft());
+            vX = -vX;
+        }
+        if (getRight() > canvas.getRight()) { //отскок от правого края окна
+            setRight(canvas.getRight());
+            vX = -vX;
+        }
+        if (getTop() < canvas.getTop()) { //отскок от верха
+            setTop(canvas.getTop());
+            vY = -vY;
+        }
+        if (getBottom() > canvas.getBottom()) { //отскоок от низа
+            setBottom(canvas.getBottom());
+            vY = -vY;
+        }
+
+    }
+
+    @Override
+    public void render(MainCanvas canvas, Graphics g) { //готовим для отрисовке на canvas
+        g.setColor(color);
+        g.fillOval((int) getLeft(), (int) getTop(),
+                (int) getWidth(), (int) getHeight());
+    }
 }
